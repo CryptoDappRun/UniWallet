a8917818d8d13740043c5f84362c5fcf647f9e0e
CryptoDappRun <122711721+CryptoDappRun@users.noreply.github.com>
2024-11-02 20:36:28 +0800

    Add files via upload

---
+++ networks/Base.js
@@ -0,0 +1,85 @@
+let BaseTokens={
+    "name": "Base Tokens",
+    "logoURI": "https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x60781C2586D68229fde47564546784ab3fACA982/logo.png",
+    "keywords": [
+        "avalanche",
+        "defi",
+        "dex"
+    ],
+    "version": {
+        "major": 5,
+        "minor": 13,
+        "patch": 0,
+        "1st is Main coin": 0,
+        "2st is Wrap Main coin": 0,
+        "3st is coin": 0
+    },
+    "timestamp": "2021-10-31T04:17:05+00:00",
+    "adapters": [
+        {
+            "name": "Aerodrome",
+            "address": "0x3Df2cf00Cb2310C0b5E7bb05C52D530338db6f0c"
+            
+            },  
+             {
+            "name": "SushiSwap",
+            "address": "0xC33f87B2AE5b1A4f5F7d3fd7F4548C38ce1959a4"
+            
+            },    
+             {
+            "name": "BaseSwap",
+            "address": "0x06De4090A37d87DC9554529A425210A99922Ad8c"
+            
+            }        
+
+
+        ],
+
+
+    "tokens": [
+        {
+            "address": "0x4200000000000000000000000000000000000006",
+            "chainId": 43114,
+            "name": "WETH",
+            "symbol": "WETH",
+            "decimals": 18,
+            "logoURI": "https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x60781C2586D68229fde47564546784ab3fACA982/logo.png"
+        }, 
+
+        {
+            "address": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+            "chainId": 43114,
+            "name": "USDC",
+            "symbol": "USDC",
+            "decimals": 6,
+            "logoURI": "https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x60781C2586D68229fde47564546784ab3fACA982/logo.png"
+        },        
+        {
+            "address": "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
+            "chainId": 43114,
+            "name": "DAI",
+            "symbol": "DAI",
+            "decimals": 18,
+            "logoURI": "https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x60781C2586D68229fde47564546784ab3fACA982/logo.png"
+        },
+        {
+            "address": "0xB6fe221Fe9EeF5aBa221c348bA20A1Bf5e73624c",
+            "chainId": 43114,
+            "name": "rETH",
+            "symbol": "rETH",
+            "decimals": 18,
+            "logoURI": "https://raw.githubusercontent.com/avaware/avaware/main/logo.png"
+        },
+        {
+            "address": "0x23ee2343B892b1BB63503a4FAbc840E0e2C6810f",
+            "chainId": 43114,
+            "name": "AXL",
+            "symbol": "AXL",
+            "decimals": 18,
+            "logoURI": "https://raw.githubusercontent.com/avaware/avaware/main/logo.png"
+        }
+    ]
+}
+
+
+
